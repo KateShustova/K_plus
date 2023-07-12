ffa512638e16c062320dc1d4fbc87918fd709f61
Daniil <daniil.sadovnikov@rambler.ru>
2023-07-12 17:02:25 +0300

    Delete consultant_project directory

--- consultant_project/consultant_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from project import views
-
-urlpatterns = [
-    path("", views.index),
-    path("documents", views.documents),
-    path("risks", views.documents),
-]

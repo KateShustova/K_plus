6fa706b424aed10f29c2fdb7920699496c4d202d
Daniilmipt <sadovnikov.diu@phystech.edu>
2023-07-19 01:03:23 +0300

    divide questions into 2 parts - legal and physical users

--- consultant/project/views.py
+++ consultant/project/views.py
@@ -1,26 +1,34 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from project.models import Poll
 import json
 
 
 def index_page(request):
-    with open('project/questions.json', encoding='utf-8') as qustion_file:
-        question_list = json.loads(qustion_file.read())['questions']
+    with open('project/questions_phys.json', encoding='utf-8') as qustion_file:
+        question_phys_list = json.loads(qustion_file.read())['questions']
+
+    with open('project/questions_legal.json', encoding='utf-8') as qustion_file:
+        question_legal_list = json.loads(qustion_file.read())['questions']
         
     with open('project/documents.json', encoding='utf-8') as document_file:
         document_list = json.loads(document_file.read())['documents']
 
-    paginator = Paginator(question_list, len(question_list)) 
-    page_num = request.GET.get('page', 1)
+    paginator_legal = Paginator(question_legal_list, len(question_legal_list))
+    page_num_legal = request.GET.get('page', 1)
+
+    paginator_phys = Paginator(question_phys_list, len(question_phys_list))
+    page_num_phys = request.GET.get('page', 1)
 
     try:
-        page_obj = paginator.page(page_num)
+        page_obj_legal = paginator_legal.page(page_num_legal)
+        page_obj_phys = paginator_phys.page(page_num_phys)
     except PageNotAnInteger:
         # if page is not an integer, deliver the first page
-        page_obj = paginator.page(1)
+        page_obj_legal = paginator_legal.page(1)
+        page_obj_phys = paginator_phys.page(1)
     except EmptyPage:
         # if the page is out of range, deliver the last page
-        page_obj = paginator.page(paginator.num_pages)
+        page_obj_legal = paginator_legal.page(page_num_legal)
+        page_obj_phys = paginator_phys.page(page_num_phys)
 
-    return render(request, 'index.html', {'page_obj': page_obj, 'docs': document_list})
+    return render(request, 'index.html', {'page_obj_legal': page_obj_legal, 'page_obj_phys': page_obj_phys, 'docs': document_list})

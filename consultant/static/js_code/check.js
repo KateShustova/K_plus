3e58603464431d94a009a3f5c6ae4dd13dc35328
Daniilmipt <sadovnikov.diu@phystech.edu>
2023-07-20 00:33:10 +0300

    fix bags

--- consultant/static/js_code/check.js
+++ consultant/static/js_code/check.js
@@ -1,6 +1,6 @@
 function check() {
     let span = document.getElementById("risks");
-    if (sessionStorage.getItem("risks") === null || JSON.parse(sessionStorage.getItem("risks")).length == 0 || document.getElementById('d-q-ans-container_0').style.display != "none") {
+    if (sessionStorage.getItem("risks") === null || JSON.parse(sessionStorage.getItem("risks")).length == 0 || document.getElementById("d-q-ans-container_0") !== null) {
         document.getElementById("risk-count").style.display = "none";
     } else {
         document.getElementById("risk-count").style.display = "block";
@@ -9,7 +9,17 @@ function check() {
         for (let i = 0; i < t.length; i++) {
             str += t[i] + ", ";
         }
-        span.innerText = "У вас " + JSON.parse(sessionStorage.getItem("risks")).length + " рисков \n Перейдите на вопросы " + str.substring(0, str.length - 2);
+        var ln = JSON.parse(sessionStorage.getItem("risks")).length;
+        if(ln >= 2 && ln <= 4){
+            var txt = "У вас " + ln + " риска \n Перейдите на вопросы " + str.substring(0, str.length - 2);
+        }
+        else if (ln === 1){
+            var txt = "У вас " + ln + " риск \n Перейдите на вопрос " + str.substring(0, str.length - 2);
+        }
+        else{
+            var txt = "У вас " + ln + " рисков \n Перейдите на вопросы " + str.substring(0, str.length - 2);
+        }
+        span.innerText = txt;
     }
     console.log(sessionStorage);
 }
--- consultant/static/js_code/update_user_type.js
+++ consultant/static/js_code/update_user_type.js
@@ -1,11 +1,10 @@
-function update_user_type(type){
-    if( !sessionStorage.getItem("user_type")){
-                            sessionStorage.setItem("user_type", type);
-                         }
-                         else{
-                            if (sessionStorage.getItem("user_type") != type){
-                               sessionStorage.clear();
-                               sessionStorage.setItem("user_type", type);
-                            }
-                         }
-                         }
+function update_user_type(type) {
+    if (!sessionStorage.getItem("user_type")) {
+        sessionStorage.setItem("user_type", type);
+    } else {
+        if (sessionStorage.getItem("user_type") != type) {
+            sessionStorage.clear();
+            sessionStorage.setItem("user_type", type);
+        }
+    }
+}

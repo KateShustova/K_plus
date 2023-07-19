5e2c652d23f05fa8d15de4a85ef469af78ba1745
Daniilmipt <sadovnikov.diu@phystech.edu>
2023-07-19 12:00:04 +0300

    update clear storage

---
+++ consultant/static/js_code/update_user_type.js
@@ -0,0 +1,11 @@
+function update_user_type(type){
+    if( !sessionStorage.getItem("user_type")){
+                            sessionStorage.setItem("user_type", type);
+                         }
+                         else{
+                            if (sessionStorage.getItem("user_type") != type){
+                               sessionStorage.clear();
+                               sessionStorage.setItem("user_type", type);
+                            }
+                         }
+                         }

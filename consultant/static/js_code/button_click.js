56dc2dcee0048a1381febad10d5563c189893226
Kirill Didus <dkvmail7@gmail.com>
2023-07-24 18:29:27 +0300

    add tests for hiding risk with right answer

--- consultant/static/js_code/button_click.js
+++ consultant/static/js_code/button_click.js
@@ -11,7 +11,7 @@ function btn_click(element, id) {
     element.onclick = function(event) {
         document.getElementById('d-q-ans-container_' + id).setAttribute('data-last-value', element.value);
         // выводим риск
-        display_risks(id);
+        display_risk_message(id);
 
         save_data_to_localstorage(id);
         pagination_button(event, id);
--- consultant/static/js_code/display_risks.js
+++ consultant/static/js_code/display_risks.js
@@ -1,3 +1,9 @@
-function display_risks(question_id){
+function display_risk_message(question_id){
+    console.log("show risk for: "+ question_id)
     document.getElementById('risk_message_'+question_id).style.display = 'block';
+}
+
+function hide_risk_message(question_id){
+    console.log("hide that shit! No risk for: "+ question_id)
+    document.getElementById('risk_message_'+question_id).style.display = 'none';
 }

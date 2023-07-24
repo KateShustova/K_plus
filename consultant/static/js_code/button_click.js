5cf315732e3c6a24a8f95bb86c9321d023c42d6c
Kirill Didus <dkvmail7@gmail.com>
2023-07-25 01:03:36 +0300

    feat: add logic for risk message header display

--- consultant/static/js_code/button_click.js
+++ consultant/static/js_code/button_click.js
@@ -1,20 +1,29 @@
 function btn_click_mark(element, id, value) {
     element.onclick = function(event) {
         document.getElementById('d-q-ans-container_' + id).setAttribute('data-last-value', element.value);
+
+        
         update_data_in_localstorage(id, value, false);
         pagination_button(event, id, value);
         check();
+
+        update_risk_header();
+        
+        
     };
 };
 
 function btn_click(element, id) {
     element.onclick = function(event) {
         document.getElementById('d-q-ans-container_' + id).setAttribute('data-last-value', element.value);
+
         // выводим риск
         display_risk_message(id,sessionStorage.getItem("user_type"));
 
         save_data_to_localstorage(id);
         pagination_button(event, id);
         check();
+        
+        update_risk_header();
     };
 };
--- consultant/static/js_code/display_risks.js
+++ consultant/static/js_code/display_risks.js
@@ -19,3 +19,46 @@ function hide_risk_message(question_id,user_type){
          document.getElementById('risk_message_legal_'+question_id).style.display = 'none';
     }
 }
+
+function update_risk_header(){
+
+    // Спрятать все заголовки
+
+    var header_id_list = [  "high_risk_message_header",
+                    "low_risk_message_header",
+                    "no_risk_but_questions_left_message_header",
+                    "no_risk_test_finished_message_header"];
+
+    for (var i = 0; i < header_id_list.length; i++) {
+        document.getElementById(header_id_list[i]).style.display = 'none';
+    }
+
+    // Показать нужный заголовок 
+    // Если рисков нет
+    if (sessionStorage.getItem("risks") === null 
+        || JSON.parse(sessionStorage.getItem("risks")).length == 0 
+        || document.getElementById("d-q-ans-container_0") !== null){
+        
+        // Если вопросы закончились
+        if (sessionStorage.getItem("user_type") == "phys"){
+            if (sessionStorage.getItem("now") == "21"){
+                console.log("show no_risk_test_finished_message_header")
+                document.getElementById('no_risk_test_finished_message_header').style.display = 'block';
+                return null;
+            }
+        }
+        else{
+            if (sessionStorage.getItem("now") == "18"){
+                console.log("show no_risk_test_finished_message_header")
+                document.getElementById('no_risk_test_finished_message_header').style.display = 'block';
+                return null;
+            }
+        }
+        console.log("show no_risk_but_questions_left_message_header")
+        document.getElementById('no_risk_but_questions_left_message_header').style.display = 'block';
+    }
+    else{
+        console.log("show high_risk_message_header")
+        document.getElementById('high_risk_message_header').style.display = 'block';
+    }
+}
